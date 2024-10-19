01a31284b2f8509b5bc3ea66129acef697080e10
cyborgWyrm <esorRevurg@gmail.com>
2024-10-19 15:38:28 -0600

    comments

--- DoublyLinkedSortedList.java
+++ DoublyLinkedSortedList.java
@@ -1,12 +1,14 @@
 
+
 public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface{
 	
+	// instance variables
 	private HurricaneRowData value;
 	private DoublyLinkedSortedList next = null;
 	private DoublyLinkedSortedList previous = null;
 	
 	
-	// constructor
+	// overloaded constructor
 	public DoublyLinkedSortedList(HurricaneRowData value) {
 		this.value = value;
 	}
@@ -37,6 +39,10 @@ public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface{
 		// should it also make next's previous this? Same for setPrevious
 		// it feels dangerous for this to be public and not set the corresponding
 		// value. you could end up with mismatching next and previous...
+		
+		// haha i had that exact problem and spent hours trying to figure out what
+		// id done
+		// im so impressive
 	}
 	
 	//Return a reference to the next DoublyLinkedSortedList
@@ -66,6 +72,7 @@ public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface{
 	
 	//Return a reference to the first DoublyLinkedSortedList element in the list
 	public DoublyLinkedSortedList getFirst() {
+		// recursively looks for the first
 		if (previous == null) {
 			return this;
 		}
@@ -76,6 +83,7 @@ public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface{
 	
 	//Return a reference to the last DoublyLinkedSortedList element in the list
 	public DoublyLinkedSortedList getLast() {
+		// recursively looks for the last
 		if (next == null) {
 			return this;
 		}
@@ -87,62 +95,68 @@ public class DoublyLinkedSortedList implements DoublyLinkedSortedListInterface{
 	//Remove the DoublyLinkedSortedList element that has toRemove as its value
 	public DoublyLinkedSortedList remove(HurricaneRowData toRemove) {
 		if (value == toRemove) {
+			// if this is the one to remove, remove this and return
 			next.setPrevious(previous);
 			previous.setNext(next);
 			return this;
 		}
 		else {
+			// if this is not it, keep looking
 			return next.remove(toRemove);
 		}
 	}
 	
 	//Insert a new DoublyLinkedSortedList element that has the given newValue in order in the list.
 	public void insert(HurricaneRowData newValue) {
+		
+		// this happens for the first value put in the list... I just have it replace the null one
 		if (this.value == null && previous == null && next == null) {
 			this.value = newValue;
-			//System.out.print(0);
 			return;
 		}
 		
+		
+		// make a new link and create a few variables
 		DoublyLinkedSortedList newLink = new DoublyLinkedSortedList(newValue);
 		int thisAce = this.value.getACE();
 		int newAce = newValue.getACE();
 		
+		// if this is the start of the list and newLink should be the new start
 		if (previous == null && thisAce < newAce) {
 			this.previous = newLink;
 			newLink.setNext(this);
-			///System.out.print(1);
 		}
+		
+		// if this is the end of the list and newLink should be the new end
 		else if (next == null && thisAce > newAce) {
 			this.next = newLink;
 			newLink.setPrevious(this);
-			//System.out.print(2);
 		}
+		
+		// if newLink should go right after this
 		else if (thisAce > newAce && next.value.getACE() < newAce) {
 			newLink.setNext(next);
 			newLink.setPrevious(this);
 			
 			next.setPrevious(newLink);
 			this.setNext(newLink);
-			
-			//System.out.print(3);
 		}
-		else if (thisAce > newAce) {
-			//System.out.print(4);
+		
+		// if we should keep looking down the list (equal ace values are put after)
+		else if (thisAce >= newAce) {
 			next.insert(newValue);
 		}
+		
+		// if we should keep looking up the list
 		else if (thisAce < newAce) {
-			//System.out.print(5);
 			previous.insert(newValue);
-		}
-		else {
-			//System.out.println(6);
 		}
 		
 	}
 	
 	//Return the entire list as a multi-line String
 	public String toString() {
+		// recursively get each line
 		String toReturn = value.toString() + "\n";
 		
 		if (next != null) {
